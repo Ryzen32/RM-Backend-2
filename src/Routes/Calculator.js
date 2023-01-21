60599d2b203b78f8e910063924522c6af90a06cb
Ryzen32 <123226234+Ryzen32@users.noreply.github.com>
2023-01-21 15:57:50 +0530

    Add files via upload

--- src/Routes/Calculator.js
+++ src/Routes/Calculator.js
@@ -3,15 +3,22 @@ const { authValidator } = require("../middleWares/authMiddleware");
 const calculatorRoute = express.Router();
 const { Calcultor } = require("../models/calculatorModel");
 
-calculatorRoute.get("/", authValidator, async (req, res) => {
+calculatorRoute.post("/", authValidator, async (req, res) => {
   const { amount, interest, year } = req.body;
 
   try {
-    const F =Math.floor( amount * (((1 + interest / 100) ** year - 1) / (interest / 100)));
+    const F = Math.floor(
+      amount * (((1 + interest / 100) ** year - 1) / (interest / 100))
+    );
     let Total_invest = Math.floor(amount * year);
     let Intrest_gained = F - Total_invest;
-    
-    res.send({ msg: "Your calculated Data", Total_maturity: F ,Total_invest,Intrest_gained});
+
+    res.send({
+      msg: "Your calculated Data",
+      Total_maturity: F,
+      Total_invest,
+      Intrest_gained,
+    });
   } catch (err) {
     res.status(500).send({ msg: "Failed to send severity" });
   }
